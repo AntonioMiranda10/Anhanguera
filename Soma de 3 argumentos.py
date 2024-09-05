71c7c1506db62278e5acab69e5a1193d331db7ad
AntonioMiranda10 <antonior.miranda10@gmail.com>
2024-09-05 20:38:54 -0300

    Add files via upload

---
+++ Soma de 3 argumentos.py
@@ -0,0 +1,11 @@
+Soma de 3 argumentos
+
+def somar(n1, n2, n3) :
+    return float(n1) + float(n2) + float(n3)
+
+n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+n3 = ("Digite o terceiro númeo: ")
+
+soma = somar(n1, n2, n3)
+print(f"Soma: {soma}")
